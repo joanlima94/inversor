9be2d367e17f0dbf991fe56dba52ad16fb33a914
Joan Lima <joanlima95@gmail.com>
2020-03-11 22:04:13 -0300

    Palavras invertendo mas não mantendo a ordem

--- src/main.c
+++ src/main.c
@@ -9,8 +9,7 @@ const int tam_buffer=100;
 
 char *inverte (char *c)
 {
-  char aux1,aux2;
-  char buffer[] = "teste";
+  char aux1;
   int i,tam=strlen(c),j;
 
   for(i=0, j = tam - 1;i<j;i++,j--)
@@ -19,17 +18,15 @@ char *inverte (char *c)
     c[i] = c[j];
     c[j] = aux1;
   }
-
   return c;
 }
 
 int main()
 {
-
-  /*char buffer[tam_buffer];
+  char buffer[tam_buffer];
   fgets(buffer, tam_buffer, stdin);
-  */
-  char buffer[] = "teste";
+
+  //char buffer[] = "Meu malvado favorito";
 
   printf("%s",inverte(buffer));
   return 0;
