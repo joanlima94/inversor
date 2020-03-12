dedf45aad8fc5e98a648e4d05f7bc1c9e28c0382
Joan Lima <joanlima95@gmail.com>
2020-03-11 21:51:55 -0300

    Primeira palavra invertida

--- src/main.c
+++ src/main.c
@@ -1,14 +1,36 @@
 /* Data de submissao:
- * Nome:
- * RA:
+ * Nome: Joan Lima Rios
+ * RA: 175870
  */
 
 #include <stdio.h>
+#include <string.h>
 const int tam_buffer=100;
 
-int main() {
-  char buffer[tam_buffer];
+char *inverte (char *c)
+{
+  char aux1,aux2;
+  char buffer[] = "teste";
+  int i,tam=strlen(c),j;
+
+  for(i=0, j = tam - 1;i<j;i++,j--)
+  {
+    aux1 = c[i];
+    c[i] = c[j];
+    c[j] = aux1;
+  }
+
+  return c;
+}
+
+int main()
+{
+
+  /*char buffer[tam_buffer];
   fgets(buffer, tam_buffer, stdin);
-  printf("%s", buffer);
+  */
+  char buffer[] = "teste";
+
+  printf("%s",inverte(buffer));
   return 0;
 }
