d0e8f7f2267541850d428381478179be809c29f5
Joan Lima <joanlima95@gmail.com>
2020-03-14 17:04:59 -0300

    Inversão de palavras com pilha, mas não mantendo a ordem

--- src/main.c
+++ src/main.c
@@ -5,29 +5,75 @@
 
 #include <stdio.h>
 #include <string.h>
-const int tam_buffer=100;
 
-char *inverte (char *c)
+const int tam_buffer=100;
+int n_elem = 0;
+char buffer[100];
+/*
+char *inverte (char *c) //Função que inverte as letras de uma palavra
 {
   char aux1;
   int i,tam=strlen(c),j;
-
-  for(i=0, j = tam - 1;i<j;i++,j--)
+  int tam_palavra=0,conta_letra=0,posicao=0;
+  while (conta_letra<strlen(c))
   {
-    aux1 = c[i];
-    c[i] = c[j];
-    c[j] = aux1;
+    if ((c[conta_letra]==' ') || (c[conta_letra]=='\0'))
+    {
+      tam_palavra = posicao;
+      for(i=0, j = tam_palavra;i<j;i++,j--)
+      {
+        aux1 = c[i];
+        c[i] = c[j];
+        c[j] = aux1;
+      }
+      posicao = 0;
+    }
+
+    conta_letra++;
+    posicao++;
+  }
+    return c;
+}
+*/
+void coloca(char c)
+{
+  if(n_elem < tam_buffer)
+  {
+    buffer[n_elem] = c;
+    n_elem++;
+  }
+}
+
+char retira()
+{
+  char c;
+  if (n_elem>0)
+  {
+    n_elem--;
+    c = buffer[n_elem];
   }
   return c;
 }
 
 int main()
 {
-  char buffer[tam_buffer];
+  int cont=0,flag =0, guarda = 0, coluna=0;
+  char c,pilha[tam_buffer];
+  int i=0;
+
   fgets(buffer, tam_buffer, stdin);
 
-  //char buffer[] = "Meu malvado favorito";
+  for (i = 0; i < strlen(buffer); i++)
+  {
+      coloca(buffer[i]);
+  }
+  for (i = 0; i < strlen(buffer); i++)
+  {
+    c = retira();
+    printf("%c",c);
+  }
 
-  printf("%s",inverte(buffer));
+
+
   return 0;
 }
